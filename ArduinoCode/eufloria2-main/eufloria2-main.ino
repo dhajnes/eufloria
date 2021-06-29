8b4533a9c3ae4c50c15b9de1e36190736661f53a
Andrej Kruzliak <kruzland@fel.cvut.cz>
2021-06-29 21:26:10 +0200

    start of the first main

---
+++ ArduinoCode/eufloria2-main/eufloria2-main.ino
@@ -0,0 +1,22 @@
+
+// setup of the soil moisture sensors
+int wetPin0 = A0;
+int wetPin1 = A1;
+int wetPin2 = A2;
+int wetPin3 = A3;
+
+int wetVal0 = 0;
+int wetVal1 = 0;
+int wetVal2 = 0;
+int wetVal3 = 0;
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
